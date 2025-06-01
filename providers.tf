e3bb4ac16aa943f43475b1bb782da0c733bb1f3a
rtrivgreg <rtrivgreg@yahoo.com>
2025-06-01 19:37:23 -0400

    initial upload

---
+++ providers.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.53.0"
+    }
+
+    github = {
+      source  = "integrations/github"
+      version = "5.25.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+
+  cloud {
+    organization = "RSHL2136"
+
+    workspaces {
+      name = "Gregoire_1"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  features {}
+}
+
+provider "github" {
+  token = var.GITHUB_TOKEN
+}
